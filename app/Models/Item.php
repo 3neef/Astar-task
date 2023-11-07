0bd70ec20afc4b0ad5cf0878163a0fd9c505e178
Mazin Hafiz <hajiki.326@hotmail.com>
2023-11-07 21:57:06 +0200

    :sparkles: add models and tables structure

---
+++ app/Models/Item.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Item extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['partition_id', 'name', 'price', 'description'];
+
+    public function partition()
+    {
+        return $this->belongsTo('App\Partition');
+    }
+}
---
+++ app/Models/Partition.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Partition extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['category_id', 'name', 'description'];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function items()
+    {
+        return $this->hasMany('App\Item');
+    }
+}
