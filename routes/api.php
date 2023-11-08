4ecb5152e5d0ede91cb35a3c2e678d6a4ae43953
Mazin Hafiz <hajiki.326@hotmail.com>
2023-11-08 20:46:54 +0200

    :white_check_mark: add auth to routes

--- routes/api.php
+++ routes/api.php
@@ -23,15 +23,21 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/register', [AuthController::class, 'register'])->middleware('role:admin');
     // categories routes
-    Route::apiResource('categories', CategoriesController::class);
-    Route::post('categories/restore/{id}', [CategoriesController::class ,'restore']);
+    Route::apiResource('categories', CategoriesController::class)->except(['index', 'show'])->middleware('role:admin');
+    Route::get('categories', [CategoriesController::class ,'index']);
+    Route::get('categories/{category}', [CategoriesController::class ,'show']);
+    Route::post('categories/restore/{id}', [CategoriesController::class ,'restore'])->middleware('role:admin');
 
     // partitions routes
-    Route::apiResource('partitions', PartitionsController::class);
-    Route::post('partitions/restore/{id}', [PartitionsController::class ,'restore']);
+    Route::apiResource('partitions', PartitionsController::class)->except(['index', 'show'])->middleware('role:admin');
+    Route::get('partitions', [PartitionsController::class ,'index']);
+    Route::get('partitions/{partition}', [PartitionsController::class ,'show']);
+    Route::post('partitions/restore/{id}', [PartitionsController::class ,'restore'])->middleware('role:admin');
 
     // Items routes
-    Route::apiResource('items', ItemsController::class);
-    Route::post('items/restore/{id}', [ItemsController::class ,'restore']);
+    Route::apiResource('items', ItemsController::class)->except(['index', 'show'])->middleware('role:admin');
+    Route::get('items', [ItemsController::class ,'index']);
+    Route::get('items/{item}', [ItemsController::class ,'show']);
+    Route::post('items/restore/{id}', [ItemsController::class ,'restore'])->middleware('role:admin');
 
 });
